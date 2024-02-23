179fa8086002610058058f175b65fda56e0153e9
Arstanbek Rakhmatulaev <rakhmatulaev_a@auca.kg>
2024-02-23 18:09:40 +0300

    Add basic design for search fragment

--- app/src/main/java/com/example/revirotask/ui/fragments/MainPageFragment.kt
+++ app/src/main/java/com/example/revirotask/ui/fragments/MainPageFragment.kt
@@ -1,7 +1,11 @@
 package com.example.revirotask.ui.fragments
 
+import android.os.Bundle
 import android.view.LayoutInflater
+import android.view.View
 import android.view.ViewGroup
+import androidx.navigation.fragment.findNavController
+import com.example.revirotask.R
 import com.example.revirotask.databinding.FragmentMainPageBinding
 
 class MainPageFragment : BaseFragment<FragmentMainPageBinding>() {
@@ -11,6 +15,18 @@ class MainPageFragment : BaseFragment<FragmentMainPageBinding>() {
         container: ViewGroup?
     ): FragmentMainPageBinding {
         return FragmentMainPageBinding.inflate(inflater, container, false)
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+
+        clickListener()
+    }
+
+    private fun clickListener() {
+        binding.btnAddCity.setOnClickListener {
+            findNavController().navigate(R.id.searchFragment)
+        }
     }
 
 
--- app/src/main/java/com/example/revirotask/ui/fragments/SearchFragment.kt
+++ app/src/main/java/com/example/revirotask/ui/fragments/SearchFragment.kt
@@ -1,7 +1,10 @@
 package com.example.revirotask.ui.fragments
 
+import android.os.Bundle
 import android.view.LayoutInflater
+import android.view.View
 import android.view.ViewGroup
+import androidx.navigation.fragment.findNavController
 import com.example.revirotask.databinding.FragmentSearchBinding
 
 class SearchFragment : BaseFragment<FragmentSearchBinding>() {
@@ -11,6 +14,19 @@ class SearchFragment : BaseFragment<FragmentSearchBinding>() {
         container: ViewGroup?
     ): FragmentSearchBinding {
         return FragmentSearchBinding.inflate(inflater, container, false)
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+        clickListeners()
+    }
+
+    private fun clickListeners() {
+        binding.apply {
+            btnGoBack.setOnClickListener {
+                findNavController().popBackStack()
+            }
+        }
     }
 
 }
